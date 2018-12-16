8d4800f9d917d5289e743bcc67850a9b1ccf9185
Myung Kim <mjk287@cornell.edu>
2018-12-16 09:55:54 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -29,5 +29,9 @@ function removeElementFromBeginningOfArray(array){
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  
+  return array.pop()
+}
+
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length-1)
 }
